99a24ddc727535ef58852b68c994d5a68ae7249e
hjunior29 <helderjuniorsilvalima@gmail.com>
2025-11-27 10:24:35 -0300

    refactor: remove all project files

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-// Entry file for Remotion
-// npx remotion render <entry-file> CaptionedVideo out/video.mp4
-
-import { registerRoot } from "remotion";
-import { RemotionRoot } from "./Root";
-
-registerRoot(RemotionRoot);
